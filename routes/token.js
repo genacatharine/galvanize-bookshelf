c433afacd67726605b96e5b0f7e06020147c62ef
Gena Israel <gena.catharine@gmail.com>
2017-10-02 13:15:31 -0600

    part 4 passed

--- routes/token.js
+++ routes/token.js
@@ -5,55 +5,75 @@ const router = express.Router();
 const humps = require('humps')
 const JWT_KEY = process.env.JWT_KEY
 const camelNew = humps.camelizeKeys('users')
+const bcrypt = require('bcrypt')
 
 
 /* GET users listing. */
 router.get('/token', (req, res) => {
-jwt.verify(req.cookies.token, JWT_KEY, (err, payload) => {
-    if (err){
-      // })
-      // }
-      // '/token', function(req, res, next) {
-      // if (!req.cookies.token) {
+  jwt.verify(req.cookies.token, JWT_KEY, (err, payload) => {
+    if (err) {
       res.send(false)
-  }
-  else {
-    res.send(true)
-  }
-})
+    } else {
+      res.send(true)
+    }
+  })
 });
 
 /* POST */
 router.post('/token', function(req, res, next) {
   knex('users')
     .where('email', req.body.email)
+    .first()
+    .returning('*')
+    //returning email, firstname, lastname, pass, id , createdat,updatedat
     .then((users) => {
-      let user = users[0];
-      delete user.hashed_password;
-      delete user.created_at;
-      delete user.updated_at;
-      let camelUser = humps.camelizeKeys(user);
-      // console.log(camelUser);
-      let cookie = jwt.sign(camelUser, process.env.JWT_KEY);
-      // console.log(cookie);
-      res.cookie('token', cookie, {
-        httpOnly: true
-      });
-      res.send(camelUser);
-    })
+      if (!users) { //if not a user send error
+        res.setHeader('Content-Type', 'text/plain')
+        res.status(400)
+        res.send('Bad email or password')
+      } else { //compare password to hashed password if it's a match set up a key and send new object
+        bcrypt.compare(req.body.password, users.hashed_password, function(err, match) {
+          if (match) {
+            let token = jwt.sign({
+              usersID: users.id
+            }, process.env.JWT_KEY);
+
+            let newObj = {
+              id: users.id,
+              email: users.email,
+              firstName: users.first_name,
+              lastName: users.last_name
+            }
+            //set a cookie
+            res.cookie('token', token, {
+              httpOnly: true
+            })
+            res.status(200) //if a match send 200 status code and object
+            res.setHeader('Content-Type', 'application/json')
+            res.send(newObj)
+          } else { //if they don't match send error
+            res.setHeader('Content-Type', 'text/plain')
+            res.status(400)
+            res.send('Bad email or password')
+          }
+
+        })
+      }
+    });
+
+
 
 
 })
 /* DELETE */
 router.delete('/token', function(req, res, next) {
   // console.log(cookie)
-  if (req.cookies.token)
-    res.clearCookie('token')
-    .then((result) => {
-      // console.log('token')
-      res.send('token')
-      res.sendStatus(200)
-    })
+  // if (req.cookies.token)
+  res.clearCookie('token')
+  // .then((result) => {
+  // console.log('token')
+  // res.send('token')
+  res.sendStatus(200)
 })
 
 module.exports = router;
