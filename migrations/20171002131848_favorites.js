82102ca4a59c4c72d35a959756beffb80a4ffdf9
Gena Israel <gena.catharine@gmail.com>
2017-10-02 16:19:58 -0600

    phase 5

---
+++ migrations/20171002131848_favorites.js
@@ -0,0 +1,14 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('favorites', function(table) {
+    table.increments()
+    table.integer('user_id').notNullable().references('id').inTable('users').onDelete('CASCADE')
+    table.integer('book_id').notNullable().references('id').inTable('books').onDelete('CASCADE')
+    table.timestamps(true, true)
+
+  })
+}
+
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('favorites')
+};
--- routes/favorites.js
+++ routes/favorites.js
@@ -1,10 +1,51 @@
 'use strict';
-
+const boom = require('boom')
+const jwt = require('jsonwebtoken')
 const express = require('express');
-
+const knex = require('../knex')
+const JWT_KEY = process.env.JWT_KEY
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
-// YOUR CODE HERE
+const {camelizeKeys, decamelizeKeys} = require('humps');
 
+const authorize = (req, res, next) => {
+  jwt.verify(req.cookies.token, JWT_KEY, (err, payload) => {
+    if (err) {
+      return next(boom.create(401, 'Unauthorized'))
+    }
+    req.claim = payload
+
+    next()
+  })
+}
+
+router.get('/favorites', authorize, function(req, res, next) {
+  knex('favorites')
+    .innerJoin('books', 'books.id', 'favorites.book_id')
+    .where('favorites.user_id', req.claim.userId)
+    .then((rows) => {
+      const favs = camelizeKeys(rows)
+
+      res.send(favs)
+    })
+    .catch((err) => {
+      next(err)
+    })
+})
+router.get('/favorites/check', authorize, function(req, res, next){
+  knex('favorites')
+  .then((rows)=>{
+    // if(row) {
+    res.status(200)
+    res.send(true)
+  
+    // res.send(false)
+})
+
+  .catch((err)=>{
+    next(err)
+  })
+
+  })
 module.exports = router;
