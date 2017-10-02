3b95a571e0ff643a4d6072b8d53ded98b0ef6c96
Gena Israel <gena.catharine@gmail.com>
2017-10-02 17:59:31 -0600

    part 5

--- routes/favorites.js
+++ routes/favorites.js
@@ -7,7 +7,10 @@ const JWT_KEY = process.env.JWT_KEY
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
-const {camelizeKeys, decamelizeKeys} = require('humps');
+const {
+  camelizeKeys,
+  decamelizeKeys
+} = require('humps');
 
 const authorize = (req, res, next) => {
   jwt.verify(req.cookies.token, JWT_KEY, (err, payload) => {
@@ -15,16 +18,19 @@ const authorize = (req, res, next) => {
       return next(boom.create(401, 'Unauthorized'))
     }
     req.claim = payload
+    // console.log(req.claim);
 
     next()
   })
 }
 
 router.get('/favorites', authorize, function(req, res, next) {
+  // console.log('token', req.claim);
   knex('favorites')
-    .innerJoin('books', 'books.id', 'favorites.book_id')
-    .where('favorites.user_id', req.claim.userId)
+    .innerJoin('books', 'favorites.book_id', '=', 'books.id')
+    .where('favorites.user_id', req.claim.usersID)
     .then((rows) => {
+      // console.log('rows', rows);
       const favs = camelizeKeys(rows)
 
       res.send(favs)
@@ -33,19 +39,52 @@ router.get('/favorites', authorize, function(req, res, next) {
       next(err)
     })
 })
-router.get('/favorites/check', authorize, function(req, res, next){
+router.get('/favorites/check', authorize, function(req, res, next) {
+
   knex('favorites')
-  .then((rows)=>{
-    // if(row) {
-    res.status(200)
-    res.send(true)
-  
-    // res.send(false)
+    .innerJoin('books', 'favorites.book_id', 'books.id')
+    // console.log(req.query)
+    .where('book_id', req.query.bookId)
+
+    .then((rows) => {
+      // console.log(req.query.bookId)
+
+      if (rows.length<0) {
+        console.log(rows)
+        res.status(200)
+        res.send(false)
+      } else {
+        res.status(200)
+        res.send(true)
+
+      }
+    })
+
+  // .catch((err) => {
+  //   next(err)
+  // })
+
 })
+router.post('/favorites', authorize, function(req, res, next) {
+  knex('favorites')
+    .insert(
+      req.body
+    )
+    .then((result) => {
+        res.status(200)
+        res.send(result)
+      }
 
-  .catch((err)=>{
-    next(err)
-  })
+    )
+})
+router.delete('/favorites', authorize, function(req, res, next) {
+  knex('favorites')
+    .del()
+    .where('book_id', req.query.bookId)
+    .then((result) => {
+  res.status(200)
+  res.send(result)
+    })
 
-  })
+})
 module.exports = router;
